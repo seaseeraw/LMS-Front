fe57c35c1c82507ff5916c5396d5aa5cb2a7fcf0
seaseeraw <seaseeraw12@gmail.com>
2024-11-14 10:55:59 +1100

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div>
+      <h1>
+        Coming soon...
+      </h1>
+     </div>
+        
+    </>
+  )
+}
+
+export default App
